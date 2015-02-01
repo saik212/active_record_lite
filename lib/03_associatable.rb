1245c0c204f3ea9e8bf709d0b55cdc15ded2ef31
Sai Kandala <kandala.sai@gmail.com>
2015-02-01 17:14:26 -0500

    Adds associations

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -54,17 +54,37 @@ module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
     # ...
+    assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+
+      options = self.class.assoc_options[name]
+      val = self.send(options.foreign_key)
+      options.model_class.where(options.primary_key => key).first
+
+    end
   end
 
   def has_many(name, options = {})
     # ...
+    assoc_options[name] = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+
+      options = self.class.assoc_options[name]
+      val = self.send(options.primary_key)
+      options.model_class.where(options.foreign_key => key).first
+
+    end
   end
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -6,5 +6,26 @@ module Associatable
 
   def has_one_through(name, through_name, source_name)
     # ...
+    define_method(name) do
+			through_options = self.class_name.assoc_options[through_name]
+			source_options = 	through_options.model_class.assoc_options[source_name]
+			through_table = through_options.table_name
+			source_table = source_options.table_name    	
+    end
+
+    results = DBConnection.execute(<<-SQL, self.id)
+    SELECT
+    	#{source_table}.*
+    FROM
+    	#{through_table}
+    JOIN
+    	#{source_table}
+    ON
+    	#{through_table}.#{source_options.foreign_key} = #{source_table}.#{source_options.primary_key}
+    WHERE
+    	#{source_table}.#{source_options.primary_key} = ?
+   SQL
+
+   parse_all(results).first
   end
 end
